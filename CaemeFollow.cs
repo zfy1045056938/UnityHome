cb8fcc6a7a4148644f9597afeb0f3a278a1e429a
VandersaRichard <zf1045056938@163.com>
2018-03-02 13:28:12 +0800

    Add files via upload

---
+++ CaemeFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaemeFollow : MonoBehaviour {
+
+	GameObject pobject;
+
+	// Use this for initialization
+	void Start () {
+		
+		pobject = GameObject.FindGameObjectWithTag ("Player");
+
+
+		//Get Position
+		pobject.transform.position = pobject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
